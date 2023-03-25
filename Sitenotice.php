8ab58c4d7b597f92ebf10c02b6c82b91bbf442ef
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-25 12:04:38 -0600

    To 0

--- Sitenotice.php
+++ Sitenotice.php
@@ -7,7 +7,7 @@ if ( $wmgSiteNoticeOptOut ) {
 }
 
 // Increment this version number whenever you change the site notice
-$wgMajorSiteNoticeID = ;
+$wgMajorSiteNoticeID = 0;
 
 /**
  * Wrap your sitenotice with <div data-nosnippet>(sitenotice)</div>
