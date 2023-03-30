2ecc5a7b07a92243de8745dc9374bd6628998390
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-30 15:59:59 -0600

    Add foreign repo for dcmultiversewiki

--- LocalWiki.php
+++ LocalWiki.php
@@ -2,6 +2,20 @@
 
 // Per-wiki settings that are incompatible with LocalSettings.php
 switch ( $wi->dbname ) {
+	case 'dcmultiversewiki':
+		$wgForeignFileRepos[] = [
+			'class' => ForeignAPIRepo::class,
+			'name' => 'miraheze',
+			'apibase' => 'https://dcmultiverse.miraheze.org/w/api.php',
+			'url' => 'https://static.miraheze.org/dcmultiversewiki',
+			'thumbUrl' => 'https://static.miraheze.org/dcmultiversewiki/thumb',
+			'hashLevels' => 2,
+			'transformVia404' => true,
+			'fetchDescription' => true,
+			'descriptionCacheExpiry' => 43200,
+			'apiThumbCacheExpiry' => 0,
+		];
+		break;
 	case 'metawiki':
 		wfLoadExtension( 'GlobalWatchlist' );
 		break;
