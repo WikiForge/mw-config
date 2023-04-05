66c6837ddc169b8100330709a9d1c131fa736400
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-04-04 20:03:06 -0600

    Use WikiForge

--- Sitenotice.php
+++ Sitenotice.php
@@ -29,15 +29,15 @@ $wgMajorSiteNoticeID = 0;
 			<table style="width: 100%;">
 				<tbody><tr><td style="font-size: 120%; border-left: 4px solid #ff1e00; background-color: #ff5200cf; padding: 10px 15px; color: whitesmoke;">
 					<div data-nosnippet style="padding-top:0.3em; padding-bottom:0.1em;">
-						<div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div>
+						<div class="floatleft"><img alt="WikiForge Logo" src="https://static.wikiforge.net/metawiki/8/88/WikiForge_Logo.svg" decoding="async" width="50" height="50"></div>
 						<div style="padding-bottom: 15px; font-size: 13pt; font-weight: bold;">
-							Miraheze has upgraded to MediaWiki 1.39. If you notice any bugs, please report them on <a href="https://meta.miraheze.org/wiki/Phabricator">Phabricator</a>, <a href="https://miraheze.org/discord">Discord</a>, or <a href="https://meta.miraheze.org/wiki/IRC">IRC</a>.
+							<...>
 						</div>
 
 						<span id="sitenotice-learnmore-button" class="oo-ui-widget oo-ui-widget-enabled oo-ui-buttonElement oo-ui-buttonElement-framed oo-ui-iconElement oo-ui-labelElement oo-ui-buttonWidget">
-							<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="https://meta.miraheze.org/wiki/Special:MyLanguage/MediaWiki/1.39">
+							<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="...">
 								<span class="oo-ui-iconElement-icon oo-ui-icon-notice"></span>
-								<span class="oo-ui-labelElement-label">{$skin->msg( 'miraheze-sitenotice-learnmore' )->escaped()}</span>
+								<span class="oo-ui-labelElement-label">{$skin->msg( 'wikiforge-sitenotice-learnmore' )->escaped()}</span>
 								<span class="oo-ui-indicatorElement-indicator oo-ui-indicatorElement-noIndicator"></span>
 							</a>
 						</span>
@@ -50,7 +50,7 @@ $wgMajorSiteNoticeID = 0;
 // }
 */
 // Specific wiki SiteNotice
-/* if ( !preg_match( '/^([0-9]|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|q)/', $wgDBname ) ) {
+/* if (...) {
 	$wgHooks['SiteNoticeAfter'][] = 'wfConditionalSiteNotice';
 
 	function wfConditionalSiteNotice( &$siteNotice, $skin ) {
@@ -64,15 +64,15 @@ $wgMajorSiteNoticeID = 0;
 			<table style="width: 100%;">
 				<tbody><tr><td style="font-size: 120%; border-left: 4px solid #ff1e00; background-color: #ff5200cf; padding: 10px 15px; color: whitesmoke;">
 					<div data-nosnippet style="padding-top:0.3em; padding-bottom:0.1em;">
-						<div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div>
+						<div class="floatleft"><img alt="WikiForge Logo" src="https://static.wikiforge.net/metawiki/8/88/WikiForge_Logo.svg" decoding="async" width="50" height="50"></div>
 						<div style="padding-bottom: 15px; font-size: 13pt; font-weight: bold;">
-							This wiki has been migrated to our new file storage software (Swift). If files appear missing for this wiki, please let us know by <a href="https://phabricator.miraheze.org/maniphest/task/edit/form/1">creating a task on Phabricator</a>. Thank you.
+							<...>
 						</div>
 
 						<span id="sitenotice-learnmore-button" class="oo-ui-widget oo-ui-widget-enabled oo-ui-buttonElement oo-ui-buttonElement-framed oo-ui-iconElement oo-ui-labelElement oo-ui-buttonWidget">
-							<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="https://meta.miraheze.org/wiki/Community_noticeboard#Note_from_SRE_Regarding_the_Swift_Migration">
+							<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="...">
 								<span class="oo-ui-iconElement-icon oo-ui-icon-notice"></span>
-								<span class="oo-ui-labelElement-label">{$skin->msg( 'miraheze-sitenotice-learnmore' )->escaped()}</span>
+								<span class="oo-ui-labelElement-label">{$skin->msg( 'wikiforge-sitenotice-learnmore' )->escaped()}</span>
 								<span class="oo-ui-indicatorElement-indicator oo-ui-indicatorElement-noIndicator"></span>
 							</a>
 						</span>
