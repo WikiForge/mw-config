c636734dde161ca8b5144a3e1523a32a92d90281
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-04-27 11:22:00 -0600

    Remove $wgForeignFileRepos from dcmultiversewiki

--- LocalWiki.php
+++ LocalWiki.php
@@ -2,21 +2,6 @@
 
 // Per-wiki settings that are incompatible with LocalSettings.php
 switch ( $wi->dbname ) {
-	case 'dcmultiversewiki':
-		$wgForeignFileRepos[] = [
-			'class' => \MediaWiki\Extension\QuickInstantCommons\Repo::class,
-			'directory' => $wgUploadDirectory,
-			'name' => 'miraheze',
-			'apibase' => 'https://dcmultiverse.miraheze.org/w/api.php',
-			'url' => 'https://static.miraheze.org/dcmultiversewiki',
-			'thumbUrl' => 'https://static.miraheze.org/dcmultiversewiki/thumb',
-			'hashLevels' => 2,
-			'transformVia404' => true,
-			'fetchDescription' => true,
-			'descriptionCacheExpiry' => 43200,
-			'abbrvThreshold' => 160,
-		];
-		break;
 	case 'metawiki':
 		wfLoadExtension( 'GlobalWatchlist' );
 		break;
