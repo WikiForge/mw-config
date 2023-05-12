41288e673ae56aed4e8d89d39d4c385971ebaebd
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-12 13:13:32 -0600

    Fix

--- LocalSettings.php
+++ LocalSettings.php
@@ -3974,7 +3974,7 @@ require_once '/srv/mediawiki/config/LocalWiki.php';
 
 // Define last - Extension message files for loading extensions
 if (
-	file_exists( __DIR__ . '/ExtensionMessageFiles' $wi->version . '.php' ) &&
+	file_exists( __DIR__ . '/ExtensionMessageFiles-' $wi->version . '.php' ) &&
 	!defined( 'MW_NO_EXTENSION_MESSAGES' )
 ) {
 	require_once __DIR__ . '/ExtensionMessageFiles-' $wi->version . '.php';
