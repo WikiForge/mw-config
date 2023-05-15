c3685a40dcb481847fa04194630027cb1d0ae1f0
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-14 19:51:58 -0600

    Fix

--- initialise/entrypoints/index.php
+++ initialise/entrypoints/index.php
@@ -5,10 +5,10 @@ define( 'MW_ENTRY_POINT', 'index' );
 require_once '/srv/mediawiki/config/initialise/WikiForgeFunctions.php';
 require WikiForgeFunctions::getMediaWiki( 'includes/WebStart.php' );
 
-if ( $wgArticlePath === '/$1' && strpos( $_SERVER['REQUEST_URI'], '/wiki/' ) !== 0 ) {
+if ( $wgArticlePath === '/$1' && strpos( strtoupper( $_SERVER['REQUEST_URI'] ), strtoupper( '/wiki/' ) ) !== 0 ) {
 	// Redirect to the same page maintaining the path
 	http_response_code( 302 );
-	header( 'Location: ' . $_SERVER['REQUEST_URI'] );
+	header( 'Location: ' . str_replace( '/wiki/', '/', $_SERVER['REQUEST_URI'] ) );
 	exit;
 } elseif ( $wgArticlePath === '/wiki/$1' && strpos( $_SERVER['REQUEST_URI'], '/wiki/' ) !== 0 ) {
 	// Redirect to the same page maintaining the path
