53f34c54d9e491f81b530c9ccca1c21ed2d49173
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-14 19:17:22 -0600

    Try handling article path based redirects in index.php

--- initialise/entrypoints/index.php
+++ initialise/entrypoints/index.php
@@ -1,3 +1,22 @@
 <?php
+
+if ( !isset( $_GET['action'] ) ) {
+	// Set an action value, so that MediaWiki
+	// doesn't automatically redirect before we
+	// do our own, as MediaWiki's will sometimes
+	// redirect everything to the main page.
+	$_GET['action'] = 'redirect';
+}
+
 require_once '/srv/mediawiki/config/initialise/WikiForgeFunctions.php';
 require WikiForgeFunctions::getMediaWiki( 'index.php' );
+
+if ( $wgArticlePath === '/$1' ) {
+	// Redirect to the same page maintaining the path
+	$output = RequestContext::getMain()->getOutput();
+	$output->redirect( $_SERVER['REQUEST_URI'], 302 );
+} elseif ( $wgArticlePath === '/wiki/$1' ) {
+	// Redirect to the same page maintaining the path
+	$output = RequestContext::getMain()->getOutput();
+	$output->redirect( '/wiki' . $_SERVER['REQUEST_URI'], 302 );
+}
