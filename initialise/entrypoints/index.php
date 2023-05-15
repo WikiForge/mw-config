c3b5d2fe32c7cf7b43de2695267eb224bc7c1e25
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-14 20:06:36 -0600

    Try normalising /w/index.php to article path

--- initialise/entrypoints/index.php
+++ initialise/entrypoints/index.php
@@ -7,13 +7,27 @@ require WikiForgeFunctions::getMediaWiki( 'includes/WebStart.php' );
 
 if ( $wgArticlePath === '/$1' && strpos( strtoupper( $_SERVER['REQUEST_URI'] ), strtoupper( '/wiki/' ) ) === 0 ) {
 	// Redirect to the same page maintaining the path
-	http_response_code( 302 );
-	header( 'Location: ' . str_replace( '/wiki/', '/', $_SERVER['REQUEST_URI'] ) );
+	header( 'Location: ' . str_replace( '/wiki/', '/', $_SERVER['REQUEST_URI'] ), true, 302 );
 	exit;
 } elseif ( $wgArticlePath === '/wiki/$1' && strpos( $_SERVER['REQUEST_URI'], '/wiki/' ) !== 0 ) {
 	// Redirect to the same page maintaining the path
-	http_response_code( 302 );
-	header( 'Location: /wiki' . $_SERVER['REQUEST_URI'] );
+	header( 'Location: /wiki' . $_SERVER['REQUEST_URI'], true, 302 );
+	exit;
+}
+
+if ( strpos( $_SERVER['REQUEST_URI'], '/w/index.php' ) !== 0 ) {
+	$queryString = $_SERVER['QUERY_STRING'];
+	$articlePath = str_replace( '$1', '', $wgArticlePath );
+
+	if ( preg_match( '/(.*)(?:^|&)title=([^&]*)(?:&|$)(.*)/', $queryString, $matches ) ) {
+		$newArgs = $matches[1] . $matches[3];
+		$redirectUrl = $articlePath . $matches['2'] . '?' . $newArgs;
+
+		header( 'Location: ' . $redirectUrl, true, 302 );
+		exit;
+	}
+
+	header( 'Location: ' . $articlePath, true, 301 );
 	exit;
 }
 
