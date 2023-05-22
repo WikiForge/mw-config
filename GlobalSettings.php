b311000e2140cc93092449beced42bf393527d1c
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-22 13:40:47 -0600

    Add

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -17,6 +17,7 @@ if ( $wgWikiForgeUseCentralAuth ) {
 	] );
 
 	$wgMWOAuthSharedUserSource = 'CentralAuth';
+	$wgOATHAuthDatabase = 'prodglobal';
 }
 
 if ( $wi->isExtensionActive( 'chameleon' ) ) {
