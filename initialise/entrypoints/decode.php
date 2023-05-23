55352cd392eb5c2168a22222b6bb3030c7f818c7
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-23 17:09:10 -0600

    Try fix

--- initialise/entrypoints/decode.php
+++ initialise/entrypoints/decode.php
@@ -6,14 +6,24 @@ $queryString = $_SERVER['QUERY_STRING'] ?? '';
 $decodedUri = urldecode( $uri );
 
 $redirectUrl = '/wiki' . $decodedUri;
+
 if ( $queryString ) {
 	$decodedQueryString = urldecode( $queryString );
-	$decodedQueryString = str_replace( '+', '_', $decodedQueryString );
+	parse_str( $decodedQueryString, $queryParameters );
 
-	$redirectUrl .= '?' . $decodedQueryString;
+	if ( isset( $queryParameters['title'] ) ) {
+		$title = $queryParameters['title'];
+		unset( $queryParameters['title'] );
+
+		$redirectUrl .= '/' . $title;
+	}
+
+	if ( !empty( $queryParameters ) ) {
+		$redirectUrl .= '?' . http_build_query( $queryParameters );
+	}
 }
 
 header( 'Location: ' . $redirectUrl, true, 302 );
-
 exit();
 ?>
+
