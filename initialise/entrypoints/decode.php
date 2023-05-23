ce5106ade77db4fc076418531633299e9872e2e8
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-23 17:47:44 -0600

    Support article path

--- initialise/entrypoints/decode.php
+++ initialise/entrypoints/decode.php
@@ -1,22 +1,31 @@
 <?php
 
+require_once '/srv/mediawiki/config/initialise/WikiForgeFunctions.php';
+require WikiForgeFunctions::getMediaWiki( 'includes/WebStart.php' );
+
 $uri = $_SERVER['REQUEST_URI'];
 $queryString = $_SERVER['QUERY_STRING'] ?? '';
 
 $decodedUri = urldecode( $uri );
 $decodedUri = str_replace( '/w/index.php', '', $decodedUri );
 
-$redirectUrl = '/wiki' . $decodedUri;
+$articlePath = str_replace( '/$1', '', $wgArticlePath );
+$redirectUrl = $articlePath . $decodedUri;
 
 if ( $queryString ) {
 	$decodedQueryString = urldecode( $queryString );
 	parse_str( $decodedQueryString, $queryParameters );
 
+	if ( isset( $queryParameters['useformat'] ) ) {
+		$_GET['useformat'] = $queryParameters['useformat'];
+		unset( $queryParameters['useformat'] );
+	}
+
 	if ( isset( $queryParameters['title'] ) ) {
 		$title = $queryParameters['title'];
 		unset( $queryParameters['title'] );
 
-		$redirectUrl = '/wiki/' . $title;
+		$redirectUrl = $articlePath . '/' . $title;
 	}
 
 	if ( !empty( $queryParameters ) ) {
