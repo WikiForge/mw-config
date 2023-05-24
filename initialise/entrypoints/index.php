445018aa66d04ce2656c3a61282d30149791d233
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-23 20:12:20 -0600

    Try to support $wgMainPageIsDomainRoot

--- initialise/entrypoints/index.php
+++ initialise/entrypoints/index.php
@@ -5,11 +5,12 @@ define( 'MW_ENTRY_POINT', 'index' );
 require_once '/srv/mediawiki/config/initialise/WikiForgeFunctions.php';
 require WikiForgeFunctions::getMediaWiki( 'includes/WebStart.php' );
 
+
 if ( $wgArticlePath === '/$1' && str_contains( strtoupper( $_SERVER['REQUEST_URI'] ), strtoupper( '/wiki/' ) ) ) {
 	// Redirect to the same page maintaining the path
 	header( 'Location: ' . str_replace( '/wiki/', '/', $_SERVER['REQUEST_URI'] ), true, 301 );
 	exit;
-} elseif ( $wgArticlePath === '/wiki/$1' && !str_contains( $_SERVER['REQUEST_URI'], '/wiki/' ) && !str_contains( $_SERVER['REQUEST_URI'], '/w/' ) ) {
+} elseif ( $wgArticlePath === '/wiki/$1' && !str_contains( $_SERVER['REQUEST_URI'], '/wiki/' ) && !str_contains( $_SERVER['REQUEST_URI'], '/w/' ) && !( $wgMainPageIsDomainRoot && $_SERVER['REQUEST_URI'] === '/' ) ) {
 	// Redirect to the same page maintaining the path
 	header( 'Location: /wiki' . $_SERVER['REQUEST_URI'], true, 301 );
 	exit;
