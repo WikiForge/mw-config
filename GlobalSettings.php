fe81fd50f3ade8ce3c3d8eb4bd98bbcb2f5ec8ee
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-29 20:54:45 -0600

    Set $wgSearchType to CirrusSearch

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -24,7 +24,7 @@ if ( $wi->isExtensionActive( 'chameleon' ) ) {
 
 if ( $wi->isExtensionActive( 'CirrusSearch' ) ) {
 	wfLoadExtension( 'Elastica' );
-	$wgDisableSearchUpdate = true;
+	$wgSearchType = 'CirrusSearch';
 	$wgCirrusSearchClusters = [
 		'default' => [
 			[
