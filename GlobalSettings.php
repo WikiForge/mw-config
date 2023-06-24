ad79afec2be785c85a79e5e2046c199d287493fc
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-06-24 14:51:08 -0600

    Add shellbox on test1

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -194,6 +194,13 @@ unset( $articlePath );
 
 $wgAllowedCorsHeaders[] = 'X-WikiForge-Debug';
 
+if ( wfHostname() === 'test1.wikiforge.net' ) {
+	$wgShellboxUrls = [
+		'default' => 'http://localhost:81/shellbox',
+	];
+}
+
+
 // AWS
 $wgAWSCredentials = [
 	'key' => $wmgAWSAccessKey,
