38e910e1fe6ed4368c35a3a76eb8d672a9a34a15
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-22 16:02:35 -0600

    Move to stubs

--- tests/stubs/WikiForgeFunctionsWrapper.php
+++ tests/stubs/WikiForgeFunctionsWrapper.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../initialise/WikiForgeFunctions.php';
+require_once __DIR__ . '/../../initialise/WikiForgeFunctions.php';
 
 class WikiForgeFunctionsWrapper extends WikiForgeFunctions {
 	private const CACHE_DIRECTORY = '/path/to/custom/cache/directory';
