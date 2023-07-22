8b54a4e346488b02d49646f21b7bb60d4d5082a7
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-22 17:02:29 -0600

    -

--- tests/stubs/WikiForgeFunctionsWrapper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../initialise/WikiForgeFunctions.php';
-
-class WikiForgeFunctionsWrapper extends WikiForgeFunctions {
-	private const CACHE_DIRECTORY = __DIR__;
-}
