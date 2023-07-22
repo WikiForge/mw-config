acad4f761f923d1a25adfb7842e91ef5fdeb95a4
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-22 13:44:59 -0600

    add get()

--- tests/stubs/SiteConfiguration.php
+++ tests/stubs/SiteConfiguration.php
@@ -6,8 +6,15 @@
 class SiteConfiguration {
 
 	/** @var array */
+	public $settings = [];
+
+	/** @var array */
 	public $suffixes = [];
 
 	/** @var array */
 	public $wikis = [];
+
+	public function get( string $setting, string $wiki ) {
+		return $this->settings[$setting][$wiki];
+	}
 }
