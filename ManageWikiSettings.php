eb3286d7af0626f40ae08da6946913aa76b25c15
Reception123 <utilizator.receptie123@gmail.com>
2023-10-23 21:35:58 +0200

    remove wgCitizenEnableSearch (removed upstream), per T35 (#123)
    * remove wgCitizenEnableSearch (removed upstream), per T35
    
    * syntax

--- ManageWikiSettings.php
+++ ManageWikiSettings.php
@@ -3266,15 +3266,6 @@ $wgManageWikiSettings = [
 		'help' => 'The color defined in the <code>theme-color</code> meta tag.',
 		'requires' => [],
 	],
-	'wgCitizenEnableSearch' => [
-		'name' => 'Citizen Enable Search',
-		'from' => 'citizen',
-		'type' => 'check',
-		'overridedefault' => true,
-		'section' => 'styling',
-		'help' => 'Enable or disable rich search suggestions',
-		'requires' => [],
-	],
 	'wgCitizenSearchGateway' => [
 		'name' => 'Citizen Search Gateway',
 		'from' => 'citizen',
@@ -3286,12 +3277,7 @@ $wgManageWikiSettings = [
 		'overridedefault' => 'mwActionApi',
 		'section' => 'styling',
 		'help' => 'Which gateway to use for fetching search suggestion',
-		'requires' => [
-			'settings' => [
-				'setting' => 'wgCitizenEnableSearch',
-				'value' => true,
-			],
-		],
+		'requires' => [],
 	],
 	'wgCitizenSearchDescriptionSource' => [
 		'name' => 'Citizen Search Description Source',
@@ -3321,12 +3307,7 @@ $wgManageWikiSettings = [
 		'overridedefault' => 6,
 		'section' => 'styling',
 		'help' => 'Max number of search suggestions',
-		'requires' => [
-			'settings' => [
-				'setting' => 'wgCitizenEnableSearch',
-				'value' => true,
-			],
-		],
+		'requires' => [],
 	],
 	'wgCitizenEnableCJKFonts' => [
 		'name' => 'Citizen Enable CJK fonts',
