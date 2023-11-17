ec424affcfa959445daecaeb2550c4de6369727a
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-11-17 09:39:22 -0600

    Remove WikiTide-specific config from LocalWiki

--- LocalWiki.php
+++ LocalWiki.php
@@ -36,46 +36,4 @@ switch ( $wi->dbname ) {
 		$wgFileStorageMonitorAWSAccessKey = $wmgAWSAccessKey;
 		$wgFileStorageMonitorAWSSecretKey = $wmgAWSAccessSecretKey;
 		break;
-	case 'metawikitide':
-		wfLoadExtensions( [
-			'GlobalWatchlist',
-			'ImportDump',
-			'IncidentReporting',
-			'RemovePII',
-			'SecurePoll',
-		] );
-		break;
-	case 'snxyzincubatorwikitide':
-		$wgLogos = [
-			'1x' => "https://static.wikiforge.net/snxyzincubatorwikitide/2/2e/Incubator_Logo.2023.svg",
-			'svg' => "https://static.wikiforge.net/snxyzincubatorwikitide/2/2e/Incubator_Logo.2023.svg",
-			'icon' => "https://static.wikiforge.net/snxyzincubatorwikitide/2/2e/Incubator_Logo.2023.svg",
-			'wordmark' => [
-				'src' => "https://static.wikiforge.net/snxyzincubatorwikitide/d/d5/Wordmark_EN.svg",
-				'1x' => "https://static.wikiforge.net/snxyzincubatorwikitide/d/d5/Wordmark_EN.svg",
-				'width' => 135,
-				'height' => 20,
-			],
-			'tagline' => [
-				'src' => "https://static.wikiforge.net/snxyzincubatorwikitide/6/60/Tagline_EN.svg",
-				'width' => 135,
-				'height' => 15,
-			],
-			'variants' => [
-				'vi' => [
-					'wordmark' => [
-						'src' => "https://static.wikiforge.net/snxyzincubatorwikitide/4/41/Wordmark_VI.svg",
-						'1x' => "https://static.wikiforge.net/snxyzincubatorwikitide/4/41/Wordmark_VI.svg",
-						'width' => 135,
-						'height' => 20,
-					],
-					'tagline' => [
-						'src' => "https://static.wikiforge.net/snxyzincubatorwikitide/1/1b/Tagline_VI.svg",
-						'width' => 135,
-						'height' => 15,
-					],
-				],
-			],
-		];
-		break;
 }
