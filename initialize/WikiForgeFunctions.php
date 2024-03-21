1abea7b2ab7e5717cab3e49cf41c11c2d16a21f9
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-03-20 20:41:22 -0500

    Pass getServiceContainer to RemoteWiki

--- initialize/WikiForgeFunctions.php
+++ initialize/WikiForgeFunctions.php
@@ -1096,7 +1096,7 @@ class WikiForgeFunctions {
 			'section' => 'main',
 		];
 
-		$wiki = new RemoteWiki( $dbName );
+		$wiki = new RemoteWiki( $dbName, $this->getServiceContainer()->get( 'CreateWikiHookRunner' ) );
 
 		if ( $permissionManager->userHasRight( $context->getUser(), 'managewiki-core' ) ) {
 			$formDescriptor['plan'] = [
